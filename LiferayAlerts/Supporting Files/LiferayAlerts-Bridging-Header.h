0b0360bb944e13dd9c61bcca63acdc4c72a45671
Silvio Santos <silvio.santos@liferay.com>
2014-10-21 16:50:32 -0300

    Add Liferay Push Notifications SDK class

---
+++ LiferayAlerts/Supporting Files/LiferayAlerts-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LiferayAlerts-Bridging-Header.h
+//  Liferay Alerts
+//
+//  Silvio Santos
+//
+#import <Foundation/Foundation.h>
+
+// Liferay services
+#import "LRCallback.h"
+#import "LRSession.h"
+#import "LRError.h"
+#import "LRJSONObjectWrapper.h"
+#import "LRPushnotificationsdeviceService_v62.h"
