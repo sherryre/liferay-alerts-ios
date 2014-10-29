c163435db96857e6b5ef733ff78524e445a18bf6
Silvio Santos <silvio.santos@liferay.com>
2014-10-29 12:20:32 -0300

    Update source code license and author format

--- LiferayAlerts/Models/User.swift
+++ LiferayAlerts/Models/User.swift
@@ -1,14 +1,22 @@
-//
-//  LiferayAlerts.swift
-//  LiferayAlerts
-//
-//  Created by Silvio Santos on 10/23/14.
-//  Copyright (c) 2014 Liferay. All rights reserved.
-//
-
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 import Foundation
 import CoreData
 
+/**
+ * @author Silvio Santos
+ */
 class User: NSManagedObject {
 
     @NSManaged var fullName: String
