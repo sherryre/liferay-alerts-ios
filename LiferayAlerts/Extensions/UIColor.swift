ebb4b2dfb1fefc287b8391b4e9ba2671b91fb8a8
Silvio Santos <silvio.santos@liferay.com>
2014-10-29 11:55:41 -0300

    Create UIColor initializer that receives a String color

---
+++ LiferayAlerts/Extensions/UIColor.swift
@@ -0,0 +1,60 @@
+//
+//  UIColors.swift
+//  LiferayAlerts
+//
+//  Silvio Santos
+//
+
+import UIKit
+
+
+extension UIColor {
+
+	convenience init(rgb: NSString) {
+		var rgb: NSString = rgb
+
+		if (rgb.hasPrefix("#")) {
+			rgb = rgb.substringFromIndex(1)
+		}
+
+		var red, green, blue, alpha : NSString
+
+		if (rgb.length == 6) {
+			alpha = "FF";
+			red = rgb.substringWithRange(NSMakeRange(0, 2))
+			green = rgb.substringWithRange(NSMakeRange(2, 2))
+			blue = rgb.substringWithRange(NSMakeRange(4, 2))
+		}
+		else if (rgb.length == 8) {
+			alpha = rgb.substringWithRange(NSMakeRange(0, 2))
+			red = rgb.substringWithRange(NSMakeRange(2, 2))
+			green = rgb.substringWithRange(NSMakeRange(4, 2))
+			blue = rgb.substringWithRange(NSMakeRange(6, 2))
+		}
+		else {
+			self.init()
+
+			return
+		}
+
+		var a: UInt32 = 0
+		var r: UInt32 = 0
+		var g: UInt32 = 0
+		var b: UInt32 = 0
+
+		NSScanner(string:alpha).scanHexInt(&a)
+		NSScanner(string:red).scanHexInt(&r)
+		NSScanner(string:green).scanHexInt(&g)
+		NSScanner(string:blue).scanHexInt(&b)
+
+		if ((r == g) && (g == b)) {
+			self.init(white:(CGFloat(r) / 255), alpha:(CGFloat(a) / 255))
+		}
+		else {
+			self.init(
+				red:(CGFloat(r) / 255), green:(CGFloat(g) / 255),
+				blue:(CGFloat(b) / 255), alpha:(CGFloat(a) / 255))
+		}
+	}
+
+}
